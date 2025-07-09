fda084db3f046de328077369e3393ad7588fe61f
Manche <upstreaming_888@live.jp>
2025-07-09 15:11:00 +0900

    Add TimeLineCreator
    TL絡みを管理するクラスの追加

---
+++ MiView/MiView/Common/TimeLine/TimeLineCreator.cs
@@ -0,0 +1,459 @@
+﻿using MiView.Common.Fonts;
+using MiView.Common.Fonts.Material;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MiView.Common.TimeLine.TimeLineCreator;
+
+namespace MiView.Common.TimeLine
+{
+    /// <summary>
+    /// タイムラインコンテナ作成・識別
+    /// </summary>
+    internal class TimeLineCreator
+    {
+        public enum TIMELINE_ELEMENT
+        {
+            UNDESIGNATED = -1,
+            /// <summary>
+            /// 投稿識別キー
+            /// </summary>
+            IDENTIFIED,
+            /// <summary>
+            /// アイコン
+            /// </summary>
+            ICON,
+            /// <summary>
+            /// ユーザー名
+            /// </summary>
+            USERNAME,
+            /// <summary>
+            /// ユーザID
+            /// </summary>
+            USERID,
+            /// <summary>
+            /// リプライ
+            /// </summary>
+            REPLAYED,
+            /// <summary>
+            /// リプライ表示
+            /// </summary>
+            REPLAYED_DISP,
+            /// <summary>
+            /// 公開範囲
+            /// </summary>
+            PROTECTED,
+            /// <summary>
+            /// 公開範囲表示
+            /// </summary>
+            PROTECTED_DISP,
+            /// <summary>
+            /// リノート
+            /// </summary>
+            RENOTED,
+            /// <summary>
+            /// リノート表示
+            /// </summary>
+            RENOTED_DISP,
+            /// <summary>
+            /// ローカル
+            /// </summary>
+            ISLOCAL,
+            /// <summary>
+            /// ローカル表示
+            /// </summary>
+            ISLOCAL_DISP,
+            /// <summary>
+            /// CW
+            /// </summary>
+            CW,
+            /// <summary>
+            /// CW表示
+            /// </summary>
+            CW_DISP,
+            /// <summary>
+            /// 投稿内容
+            /// </summary>
+            DETAIL,
+            /// <summary>
+            /// 投稿日時
+            /// </summary>
+            UPDATEDAT,
+            /// <summary>
+            /// 投稿元インスタンス
+            /// </summary>
+            SOURCE,
+            /// <summary>
+            /// 投稿元ソフトウェア情報
+            /// </summary>
+            SOFTWARE,
+            /// <summary>
+            /// 投稿元オリジナルjson情報
+            /// </summary>
+            ORIGINAL,
+        }
+
+        /// <summary>
+        /// 非表示対象
+        /// </summary>
+        public static TIMELINE_ELEMENT[] _DisabledElements = new TIMELINE_ELEMENT[]
+        {
+            TIMELINE_ELEMENT.CW,
+            TIMELINE_ELEMENT.UNDESIGNATED,
+            TIMELINE_ELEMENT.IDENTIFIED,
+            TIMELINE_ELEMENT.RENOTED,
+            TIMELINE_ELEMENT.PROTECTED,
+            TIMELINE_ELEMENT.ISLOCAL,
+            TIMELINE_ELEMENT.REPLAYED,
+            TIMELINE_ELEMENT.ORIGINAL
+        };
+
+        public List<TimeLineContainer> TimeLineData = new List<TimeLineContainer>();
+
+        public TimeLineCreator()
+        {
+        }
+    }
+
+    /// <summary>
+    /// タイムラインオブジェクト
+    /// </summary>
+    internal class TimeLineContainer
+    {
+        public TimeLineContainer() { }
+
+        public enum PROTECTED_STATUS
+        {
+            Public,
+            SemiPublic,
+            Home,
+            Follower,
+            Direct,
+        }
+
+        public string IDENTIFIED { get; set; } = string.Empty;
+        public string ICON { get; set; } = string.Empty;
+        public string USERNAME { get; set; } = string.Empty;
+        public string USERID { get; set; } = string.Empty;
+        public bool REPLAYED { get; set; } = false;
+        public string? REPLAYED_DISP {  get; set; }
+        public PROTECTED_STATUS PROTECTED { get; set; } = PROTECTED_STATUS.Public;
+        public string? PROTECTED_DISP { get; set; }
+        public bool RENOTED { get; set; } = false;
+        public string? RENOTED_DISP { get; set; }
+        public bool ISLOCAL { get; set; } = false;
+        public string? ISLOCAL_DISP {  get; set; }
+        public bool CW {  get; set; } = false;
+        public string? CW_DISP { get; set; }
+        public string DETAIL { get; set; } = string.Empty;
+        public string CONTENT {  get; set; } = string.Empty;
+        public string UPDATEDAT { get; set; } = string.Empty;
+        public string SOURCE { get; set; } = string.Empty;
+        public string SOFTWARE { get; set; } = string.Empty;
+        public string ORIGINAL { get; set; } = string.Empty;
+    }
+
+    partial class DataGridTimeLine : System.Windows.Forms.DataGridView
+    {
+        /// <summary>
+        /// 空文字
+        /// </summary>
+        private static string _Common_Empty = string.Empty;
+
+        /// <summary>
+        /// メンション(あっとまーく)
+        /// </summary>
+        private static string _Common_Alternate_Email = MaterialIcons.AlternateEmail;
+
+        /// <summary>
+        /// RN
+        /// </summary>
+        private static string _Common_Repeat = MaterialIcons.Repeat;
+
+        /// <summary>
+        /// パブリック
+        /// </summary>
+        private static string _Common_Public = MaterialIcons.Language;
+        /// <summary>
+        /// はなモード・セミパブリック
+        /// </summary>
+        private static string _Common_Wifi = MaterialIcons.Wifi;
+        /// <summary>
+        /// ホーム
+        /// </summary>
+        private static string _Common_Home = MaterialIcons.Home;
+        /// <summary>
+        /// DM
+        /// </summary>
+        private static string _Common_Direct = MaterialIcons.Mail;
+        /// <summary>
+        /// フォロワー
+        /// </summary>
+        private static string _Common_Locked = MaterialIcons.Lock;
+
+        /// <summary>
+        /// 連合
+        /// </summary>
+        private static string _Common_Rocket_Launch = MaterialIcons.RocketLaunch;
+        /// <summary>
+        /// ローカルのみ
+        /// </summary>
+        /// <remarks>
+        /// 赤字にすること
+        /// </remarks>
+        private static string _Common_Rocket = MaterialIcons.Rocket;
+
+        /// <summary>
+        /// CW表示
+        /// </summary>
+        private static string _Common_Visibility_Off = MaterialIcons.VisibilityOff;
+
+        /// <summary>
+        /// 列幅
+        /// </summary>
+        private static Dictionary<TimeLineCreator.TIMELINE_ELEMENT, int> _ColumWidths = new Dictionary<TIMELINE_ELEMENT, int>()
+        {
+            { TIMELINE_ELEMENT.UNDESIGNATED, -1 },
+            { TIMELINE_ELEMENT.ICON, 20 },
+            { TIMELINE_ELEMENT.USERNAME, 60 },
+            { TIMELINE_ELEMENT.USERID, 60 },
+            { TIMELINE_ELEMENT.REPLAYED_DISP, 20 },
+            { TIMELINE_ELEMENT.PROTECTED_DISP, 20 },
+            { TIMELINE_ELEMENT.ISLOCAL_DISP, 20 },
+            { TIMELINE_ELEMENT.RENOTED_DISP, 20 },
+            { TIMELINE_ELEMENT.CW_DISP, 20 },
+            { TIMELINE_ELEMENT.DETAIL, 350 },
+            { TIMELINE_ELEMENT.SOFTWARE, 40 },
+            { TIMELINE_ELEMENT.UPDATEDAT, 140 },
+            { TIMELINE_ELEMENT.SOURCE, 60 }
+        };
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public DataGridTimeLine()
+        {
+            // 初期設定
+            var DefaultMaterialFont = new FontLoader().LoadFontFromFile(FontLoader.FONT_SELECTOR.MATERIALICONS, 8);
+            foreach (string ColName in Enum.GetNames(typeof(TimeLineCreator.TIMELINE_ELEMENT)))
+            {
+                DataGridViewColumn Col = new DataGridViewColumn();
+                Col.Name = ColName;
+                Col.CellTemplate = new DataGridViewTextBoxCell();
+
+                // 列幅
+                if (_ColumWidths.ContainsKey((TimeLineCreator.TIMELINE_ELEMENT)Enum.Parse(typeof(TimeLineCreator.TIMELINE_ELEMENT), ColName)))
+                {
+                    Col.Width = _ColumWidths[(TimeLineCreator.TIMELINE_ELEMENT)Enum.Parse(typeof(TimeLineCreator.TIMELINE_ELEMENT), ColName)];
+                }
+
+                // マーク列
+                if (ColName == TimeLineCreator.TIMELINE_ELEMENT.REPLAYED_DISP.ToString() ||
+                    ColName == TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP.ToString() ||
+                    ColName == TimeLineCreator.TIMELINE_ELEMENT.ISLOCAL_DISP.ToString() ||
+                    ColName == TimeLineCreator.TIMELINE_ELEMENT.RENOTED_DISP.ToString() ||
+                    ColName == TimeLineCreator.TIMELINE_ELEMENT.CW_DISP.ToString())
+                {
+                    Col.DefaultCellStyle.Font = DefaultMaterialFont;
+                }
+                // 制御列
+                if (TimeLineCreator._DisabledElements.Contains((TimeLineCreator.TIMELINE_ELEMENT)Enum.Parse(typeof(TimeLineCreator.TIMELINE_ELEMENT), ColName)))
+                {
+                    Col.Visible = false;
+                }
+                this.Columns.Add(Col);
+            }
+        }
+
+        /// <summary>
+        /// 行挿入
+        /// </summary>
+        /// <param name="Container"></param>
+        public void InsertTimeLineData(TimeLineContainer Container)
+        {
+            // 行挿入
+            this.Rows.Insert(0);
+
+            // 基本行高さ
+            this.Rows[0].Height = 20;
+
+            // フォントは行ごとに定義する
+            // defaultだと反映されない
+            var DefaultMaterialFont = new FontLoader().LoadFontFromFile(FontLoader.FONT_SELECTOR.MATERIALICONS, 12);
+            this.Rows[0].Cells[(int)TIMELINE_ELEMENT.REPLAYED_DISP].Style.Font = DefaultMaterialFont;
+            this.Rows[0].Cells[(int)TIMELINE_ELEMENT.ISLOCAL_DISP].Style.Font = DefaultMaterialFont;
+            this.Rows[0].Cells[(int)TIMELINE_ELEMENT.PROTECTED_DISP].Style.Font = DefaultMaterialFont;
+            this.Rows[0].Cells[(int)TIMELINE_ELEMENT.RENOTED_DISP].Style.Font = DefaultMaterialFont;
+            this.Rows[0].Cells[(int)TIMELINE_ELEMENT.CW_DISP].Style.Font = DefaultMaterialFont;
+
+            // カラム別処理
+            foreach (string ColName in Enum.GetNames(typeof(TimeLineCreator.TIMELINE_ELEMENT)))
+            {
+                var Prop = typeof(TimeLineContainer).GetProperty(ColName);
+                if (Prop == null)
+                {
+                    continue;
+                }
+                var PropVal = Prop.GetValue(Container);
+
+                if (PropVal != null)
+                {
+                    this.Rows[0].Cells[ColName].Value = PropVal;
+                }
+
+                this.ArrangeTimeLine(0, (int)Enum.Parse(typeof(TimeLineCreator.TIMELINE_ELEMENT), ColName));
+
+                var Row = this.Rows[0];
+
+                // 色変更
+                this.ChangeDispColor(ref Row, Container);
+            }
+        }
+
+        /// <summary>
+        /// タイムライン整形
+        /// </summary>
+        /// <param name="RowIndex"></param>
+        /// <param name="ColumnIndex"></param>
+        private void ArrangeTimeLine(int RowIndex, int ColumnIndex)
+        {
+            if (RowIndex == -1)
+            {
+                return;
+            }
+            if (ColumnIndex != (int)TimeLineCreator.TIMELINE_ELEMENT.REPLAYED &&
+                ColumnIndex != (int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED &&
+                ColumnIndex != (int)TimeLineCreator.TIMELINE_ELEMENT.ISLOCAL &&
+                ColumnIndex != (int)TimeLineCreator.TIMELINE_ELEMENT.RENOTED &&
+                ColumnIndex != (int)TimeLineCreator.TIMELINE_ELEMENT.CW)
+            {
+                return;
+            }
+
+            var CellValue = this.Rows[RowIndex].Cells[ColumnIndex].Value;
+            if (CellValue == null)
+            {
+                CellValue = string.Empty;
+            }
+            switch (ColumnIndex)
+            {
+                case (int)TimeLineCreator.TIMELINE_ELEMENT.REPLAYED:
+                    this.Rows[RowIndex].Cells[TimeLineCreator.TIMELINE_ELEMENT.REPLAYED_DISP.ToString()].Value
+                            = (bool)CellValue ? _Common_Alternate_Email : _Common_Empty;
+                    this.Rows[RowIndex].Cells[TimeLineCreator.TIMELINE_ELEMENT.REPLAYED_DISP.ToString()].ToolTipText
+                            = (bool)CellValue ? "リプライ" : "";
+                    this.Rows[RowIndex].Cells[TimeLineCreator.TIMELINE_ELEMENT.REPLAYED_DISP.ToString()].Style.ForeColor
+                            = (bool)CellValue ? Color.Orange : Color.Red;
+                    break;
+                case (int)TimeLineCreator.TIMELINE_ELEMENT.CW:
+                    if ((bool)CellValue)
+                    {
+                        // CWはdetailに突っ込む時に処理させる
+                        this.Rows[RowIndex].Cells[TimeLineCreator.TIMELINE_ELEMENT.CW_DISP.ToString()].Value = _Common_Visibility_Off;
+                        this.Rows[RowIndex].Cells[TimeLineCreator.TIMELINE_ELEMENT.CW_DISP.ToString()].ToolTipText = "CW";
+                    }
+                    break;
+                case (int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED:
+                    switch ((TimeLineContainer.PROTECTED_STATUS)CellValue)
+                    {
+                        case TimeLineContainer.PROTECTED_STATUS.Public:
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].Value
+                                    = _Common_Public;
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].ToolTipText
+                                    = "パブリック";
+                            break;
+                        case TimeLineContainer.PROTECTED_STATUS.SemiPublic:
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].Value
+                                    = _Common_Wifi;
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].ToolTipText
+                                    = "セミパブリック";
+                            break;
+                        case TimeLineContainer.PROTECTED_STATUS.Home:
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].Value
+                                    = _Common_Home;
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].ToolTipText
+                                    = "ホーム";
+                            break;
+                        case TimeLineContainer.PROTECTED_STATUS.Direct:
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].Value
+                                    = _Common_Direct;
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].ToolTipText
+                                    = "ダイレクトメッセージ";
+                            break;
+                        case TimeLineContainer.PROTECTED_STATUS.Follower:
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].Value
+                                    = _Common_Locked;
+                            this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.PROTECTED_DISP].ToolTipText
+                                    = "フォロワー";
+                            break;
+                    }
+                    break;
+                case (int)TimeLineCreator.TIMELINE_ELEMENT.ISLOCAL:
+                    this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.ISLOCAL_DISP].Value
+                            = (bool)CellValue ? _Common_Rocket : _Common_Rocket_Launch;
+                    this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.ISLOCAL_DISP].ToolTipText
+                            = (bool)CellValue ? "ローカルのみ" : "連合";
+                    this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.ISLOCAL_DISP].Style.ForeColor
+                            = (bool)CellValue ? Color.Red : Color.Green;
+                    break;
+                case (int)TimeLineCreator.TIMELINE_ELEMENT.RENOTED:
+                    this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.RENOTED_DISP].Value
+                            = (bool)CellValue ? _Common_Repeat : _Common_Empty;
+                    this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.RENOTED_DISP].ToolTipText
+                            = (bool)CellValue ? "リノート" : "";
+                    this.Rows[RowIndex].Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.RENOTED_DISP].Style.ForeColor
+                            = (bool)CellValue ? Color.Green : Color.Red;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 文字色変更
+        /// </summary>
+        /// <param name="Row"></param>
+        /// <param name="Container"></param>
+        private void ChangeDispColor(ref DataGridViewRow Row, TimeLineContainer Container)
+        {
+            if (Container.RENOTED)
+            {
+                this.ChangeDispFgColorCommon(ref Row, Color.Green);
+            }
+            if (Container.REPLAYED)
+            {
+                this.ChangeDispBgColorCommon(ref Row, Color.Beige);
+            }
+            if (Container.CW)
+            {
+                this.ChangeDispBgColorCommon(ref Row, Color.LightGray);
+            }
+        }
+
+        /// <summary>
+        /// フロント文字色変更
+        /// </summary>
+        /// <param name="Row"></param>
+        /// <param name="DesignColor"></param>
+        private void ChangeDispFgColorCommon(ref DataGridViewRow Row, Color DesignColor)
+        {
+            Row.Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.USERNAME].Style.ForeColor = DesignColor;
+            Row.Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.USERID].Style.ForeColor = DesignColor;
+            Row.Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.DETAIL].Style.ForeColor = DesignColor;
+            Row.Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.UPDATEDAT].Style.ForeColor = DesignColor;
+            Row.Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.SOURCE].Style.ForeColor = DesignColor;
+            Row.Cells[(int)TimeLineCreator.TIMELINE_ELEMENT.SOFTWARE].Style.ForeColor = DesignColor;
+        }
+
+        /// <summary>
+        /// 背景文字色変更
+        /// </summary>
+        /// <param name="Row"></param>
+        /// <param name="DesignColor"></param>
+        private void ChangeDispBgColorCommon(ref DataGridViewRow Row, Color DesignColor)
+        {
+            Row.DefaultCellStyle.BackColor = DesignColor;
+        }
+    }
+}
