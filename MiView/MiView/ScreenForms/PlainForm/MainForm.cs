c3f215772d772ff63f2003246a58cb3e307e71eb
Manche <upstreaming_888@live.jp>
2025-07-12 12:59:08 +0900

    Update MainForm
    不要な初期タブを削除

--- MiView/MiView/ScreenForms/PlainForm/MainForm.cs
+++ MiView/MiView/ScreenForms/PlainForm/MainForm.cs
@@ -38,30 +38,6 @@ namespace MiView
         {
             _TimeLineManage.CreateTimeLine(ref this.MainFormObj, "Main", "tpMain");
 
-            _TimeLineManage.CreateTimeLineTab(ref this.MainFormObj, "misskeyio", "misskey.io");
-            _TimeLineManage.CreateTimeLine(ref this.MainFormObj, "misskeyio", "misskeyio");
-
-            _TimeLineManage.CreateTimeLineTab(ref this.MainFormObj, "misskeysj", "misskey.io");
-            _TimeLineManage.CreateTimeLine(ref this.MainFormObj, "misskeysj", "misskeysj");
-
-            // _TimeLineManage.CreateTimeLine(ref this.MainFormObj, "misskey.io", "tpMain");
-            // TimeLineManage.DeleteTimeLine(ref this.MainFormObj, "Main", "tpMain");
-
-
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { PROTECTED = TimeLineContainer.PROTECTED_STATUS.Public });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { PROTECTED = TimeLineContainer.PROTECTED_STATUS.SemiPublic });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { PROTECTED = TimeLineContainer.PROTECTED_STATUS.Home });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { PROTECTED = TimeLineContainer.PROTECTED_STATUS.Follower });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { PROTECTED = TimeLineContainer.PROTECTED_STATUS.Direct });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { RENOTED = true, DETAIL = "リノート" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { REPLAYED = true, DETAIL = "リプライ" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { ISLOCAL = true, DETAIL = "連合" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { USERNAME = "ほげほげ" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { USERID = "ANKIMO" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { DETAIL = "DataGridViewの行追加イベントがあんまりにもおたんこなす" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { SOFTWARE = "misskey 2024.1.0" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { SOURCE = "misskey.niri.la" });
-            //this.dataGridTimeLine1.InsertTimeLineData(new TimeLineContainer() { UPDATEDAT = "2000/01/01 01:01:01" });
         }
 
         public void AddTimeLine(string InstanceURL, string TabName, string APIKey)
